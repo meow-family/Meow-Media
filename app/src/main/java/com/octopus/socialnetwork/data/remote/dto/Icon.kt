c2507987587f2a1199ce7f1d0e51fc496e82af87
SHAMARDN <shamardn@gmail.com>
2022-11-28 18:23:22 +0200

    add DTOs necessary for Api service functions

---
+++ app/src/main/java/com/octopus/socialnetwork/data/remote/dto/Icon.kt
@@ -0,0 +1,16 @@
+package com.octopus.socialnetwork.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class Icon(
+    @SerializedName("topbar")
+    val topBar: String?,
+    @SerializedName("small")
+    val small: String?,
+    @SerializedName("smaller")
+    val smaller: String?,
+    @SerializedName("larger")
+    val larger: String?,
+    @SerializedName("large")
+    val large: String?,
+)
