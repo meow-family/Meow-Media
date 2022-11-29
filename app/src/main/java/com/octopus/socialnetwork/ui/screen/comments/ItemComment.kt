23749cb83c52cf0707e29e349820fc9f9b203e54
abdbashar <abdullahbashar98@gmail.com>
2022-11-29 19:30:05 +0300

    implement item comment UI

--- app/src/main/java/com/octopus/socialnetwork/ui/screen/comments/ItemComment.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/screen/comments/ItemComment.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
-import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,13 +21,13 @@ import com.octopus.socialnetwork.ui.theme.light_outline
 
 @Preview
 @Composable
-fun PostContent() {
+fun ItemComment(){
     ConstraintLayout(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
             .background(Color.White)
-            .padding(16.dp, 0.dp, 50.dp, 16.dp)
+            .padding(16.dp, 0.dp, 50.dp, 18.dp)
     ) {
         val userImage = createRef()
         val fullName = createRef()
@@ -44,21 +43,21 @@ fun PostContent() {
                 .wrapContentSize()
                 .clip(CircleShape)
                 .constrainAs(userImage) {
-                    top.linkTo(parent.top, 32.dp)
+                    top.linkTo(parent.top)
                     start.linkTo(parent.start, 16.dp)
                 }
         ) {
             image(painterResource(id = R.drawable.static_user_image), size = 50)
         }
-        Text(text = "Rawed Sama",
+        Text(text = "Alexa Marie",
             fontSize = 14.sp,
             color = Color.Black,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.constrainAs(fullName) {
-                top.linkTo(parent.top, 36.dp)
+                top.linkTo(parent.top, 8.dp)
                 start.linkTo(userImage.end, 8.dp)
             })
-        Text(text = "@asoo__chan",
+        Text(text = "@alexa_maire",
             fontSize = 12.sp,
             color = light_outline,
             fontWeight = FontWeight.Normal,
@@ -131,19 +130,6 @@ fun PostContent() {
                     top.linkTo(postText.bottom, 12.dp)
                     start.linkTo(reply.end, 18.dp)
                 })
-
-
-        Divider(
-            modifier = Modifier
-                .fillMaxWidth()
-                .alpha(.1f)
-                .constrainAs(dividerLine) {
-                    top.linkTo(like.bottom, 16.dp)
-                    start.linkTo(parent.start, 18.dp)
-                },
-            color = Color.Black,
-            thickness = 1.dp
-        )
 
     }
 }
