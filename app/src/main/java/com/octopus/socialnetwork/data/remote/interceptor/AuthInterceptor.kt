58511d54436fe577ddb1b968772de2bf1aa33a1c
SHAMARDN <shamardn@gmail.com>
2022-11-29 21:32:14 +0200

    implement AuthInterceptor.kt

--- app/src/main/java/com/octopus/socialnetwork/data/remote/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/octopus/socialnetwork/data/remote/interceptor/AuthInterceptor.kt
@@ -1,12 +1,21 @@
 package com.octopus.socialnetwork.data.remote.interceptor
 
+import com.octopus.socialnetwork.BuildConfig.API_KEY
 import okhttp3.Interceptor
 import okhttp3.Response
 import javax.inject.Inject
 
-
 class AuthInterceptor @Inject constructor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        TODO("Not yet implemented")
+        val original = chain.request()
+            .url
+            .newBuilder()
+            .addQueryParameter(API_KEY_QP, API_KEY)
+            .build()
+        return chain.proceed(chain.request().newBuilder().url(original).build())
+    }
+
+    companion object{
+        private const val API_KEY_QP = "api_key"
     }
 }
