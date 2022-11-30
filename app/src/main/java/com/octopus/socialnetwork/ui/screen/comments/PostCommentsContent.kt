fbe2d8449b27b3d2cc422573ac83cbe3f859b3ae
abdbashar <abdullahbashar98@gmail.com>
2022-11-30 20:21:16 +0300

    fix issue in edit text field

--- app/src/main/java/com/octopus/socialnetwork/ui/screen/comments/PostCommentsContent.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/screen/comments/PostCommentsContent.kt
@@ -6,11 +6,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Card
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -25,6 +31,19 @@ fun PostCommentsContent(state: CommentsUiState) {
 
     ) {
 
+        LazyColumn(
+            Modifier
+                .fillMaxWidth()
+                .weight(.8f),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+            items(state.comments) {
+                ItemComment(commentDetails = it)
+            }
+        }
+
         Column(
             modifier = Modifier
                 .fillMaxWidth()
@@ -36,23 +55,15 @@ fun PostCommentsContent(state: CommentsUiState) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(44.dp),
-                backgroundColor = Color.Gray,
+                backgroundColor = Color.LightGray,
                 shape = RoundedCornerShape(57.dp),
                 elevation = 0.dp
             ) {
-
-            }
-
-        }
-
-        LazyColumn(
-            Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-
-            items(state.comments){
-                ItemComment(commentDetails = it)
+                    val textState = remember { mutableStateOf(TextFieldValue()) }
+                    BasicTextField(
+                        modifier = Modifier.fillMaxSize().padding(12.dp),
+                        value = textState.value,
+                        onValueChange = {textState.value = it})
             }
         }
 
