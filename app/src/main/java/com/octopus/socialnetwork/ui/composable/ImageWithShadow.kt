3c2d2210b09b7a0e4b1e5e7f53b3203d32adbf95
Asia sama <sehunexo710@gmail.com>
2022-12-01 19:59:05 +0300

    fix background cover of profile screen

--- app/src/main/java/com/octopus/socialnetwork/ui/composable/ImageWithShadow.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/composable/ImageWithShadow.kt
@@ -2,6 +2,7 @@ package com.octopus.socialnetwork.ui.composable
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
@@ -16,6 +17,8 @@ fun ImageWithShadow(painter: Painter, modifier: Modifier){
         modifier = modifier
     ) {
         Image(
+            modifier = Modifier
+                .fillMaxSize(),
             painter = painter,
             contentDescription = stringResource(R.string.background),
             contentScale = ContentScale.Crop,
