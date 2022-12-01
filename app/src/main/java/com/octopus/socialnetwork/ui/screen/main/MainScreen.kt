80f0fff331395a89621d63508622e1bed31ffce1
ameeramjed <pr.ameer98@gmail.com>
2022-12-01 13:29:55 +0300

    set default screen

--- app/src/main/java/com/octopus/socialnetwork/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/screen/main/MainScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.pager.ExperimentalPagerApi
+import com.octopus.socialnetwork.ui.screen.register.RegisterScreen
 import com.octopus.socialnetwork.ui.theme.SocialNetworkTheme
 
 @ExperimentalPagerApi
@@ -13,7 +14,7 @@ fun SocialNetworkApp() {
     SocialNetworkTheme {
         Scaffold {
         //  HomeScreen()
-           // RegisterScreen()
+            RegisterScreen()
 //            LoginScreen()
         }
     }
