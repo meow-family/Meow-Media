a2aa02aa93fb2f794cec4bc58b17a0f31d6a590e
SHAMARDN <shamardn@gmail.com>
2022-12-03 23:57:28 +0200

    add NotificationsScreen compose

---
+++ app/src/main/java/com/octopus/socialnetwork/ui/screen/notifications/NotificationsScreen.kt
@@ -0,0 +1,61 @@
+package com.octopus.socialnetwork.ui.screen.notifications
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.octopus.socialnetwork.ui.composable.notifications.ItemNotification
+import com.octopus.socialnetwork.ui.composable.notifications.NotificationAppBar
+import com.octopus.socialnetwork.ui.screen.notifications.state.NotificationsUiState
+import com.octopus.socialnetwork.ui.theme.DividerColor
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun NotificationsScreen(
+    viewModel: NotificationsViewModel = hiltViewModel(),
+) {
+
+    val state by viewModel.state.collectAsState()
+    NotificationsContent(
+        state = state,
+    )
+}
+
+
+@Composable
+private fun NotificationsContent(
+    state: NotificationsUiState,
+) {
+    Column(
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+    ) {
+
+        NotificationAppBar()
+
+        Divider(color = DividerColor, thickness = 1.dp)
+
+        LazyColumn(modifier = Modifier
+                .fillMaxWidth(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(state.notifications){
+                ItemNotification()
+            }
+        }
+    }
+}
