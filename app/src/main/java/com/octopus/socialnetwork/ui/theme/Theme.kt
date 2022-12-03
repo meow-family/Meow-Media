8d6a60cfc44cba8d15e628745d8d9009010995ac
ameeramjed <pr.ameer98@gmail.com>
2022-12-03 13:21:30 +0300

    add extra colors in material theme

--- app/src/main/java/com/octopus/socialnetwork/ui/theme/Theme.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/theme/Theme.kt
@@ -1,31 +1,21 @@
 package com.octopus.socialnetwork.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = lightPrimaryColor,
-    onPrimary = lightOnPrimaryColor,
 
-    secondary = lightSecondaryColor,
-    onSecondary =lightOnSecondaryColor,
-    secondaryVariant = lightSecondaryVariantColor,
-
-    background = lightBackgroundColor,
-    onBackground = White,
-
-    error = errorColor,
-)
-private val LightColorPalette = lightColors(
     primary = darkPrimaryColor,
     onPrimary = darkOnPrimaryColor,
 
     secondary = darkSecondaryColor,
-    onSecondary =darkOnSecondaryColor,
+    onSecondary = darkOnSecondaryColor,
     secondaryVariant = darkSecondaryVariantColor,
 
     background = darkBackgroundColor,
@@ -33,6 +23,25 @@ private val LightColorPalette = lightColors(
 
     error = errorColor,
 )
+private val LightColorPalette = lightColors(
+    primary = lightPrimaryColor,
+    onPrimary = lightOnPrimaryColor,
+
+    secondary = lightSecondaryColor,
+    onSecondary = lightOnSecondaryColor,
+    secondaryVariant = lightSecondaryVariantColor,
+
+    background = lightBackgroundColor,
+    onBackground = White,
+
+    error = errorColor,
+)
+
+val Colors.textSecondaryColor: Color
+    get() = if (isLight) lightTextSecondaryColor else darkTextSecondaryColor
+
+val Colors.textThirdColor: Color
+    get() = if (isLight) lightTextThirdColor else darkTextThirdColor
 
 @Composable
 fun SocialNetworkTheme(
