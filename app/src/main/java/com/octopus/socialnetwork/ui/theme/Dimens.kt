b6f6a8d7efe25363a5ad4f3488808005ffd8a4bd
ameeramjed <pr.ameer98@gmail.com>
2022-12-03 04:33:28 +0300

    add dimens spacing design

---
+++ app/src/main/java/com/octopus/socialnetwork/ui/theme/Dimens.kt
@@ -0,0 +1,14 @@
+package com.octopus.socialnetwork.ui.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+val zero: Dp = 0.dp
+
+val spacing: Dp = 4.dp
+val spacingSmall: Dp = 8.dp
+val spacingMedium: Dp = 16.dp
+val spacingLarge: Dp = 24.dp
+val spacingExtraLarge: Dp = 32.dp
+val spacingHuge: Dp = 48.dp
