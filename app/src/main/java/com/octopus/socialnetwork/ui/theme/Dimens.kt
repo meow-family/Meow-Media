7b761d966dee13069e20d223eaa75c8a07c501db
ameeramjed <pr.ameer98@gmail.com>
2022-12-03 13:18:30 +0300

    add font size dimens

--- app/src/main/java/com/octopus/socialnetwork/ui/theme/Dimens.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/theme/Dimens.kt
@@ -1,7 +1,9 @@
 package com.octopus.socialnetwork.ui.theme
 
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 val zero: Dp = 0.dp
@@ -12,3 +14,14 @@ val spacingMedium: Dp = 16.dp
 val spacingLarge: Dp = 24.dp
 val spacingExtraLarge: Dp = 32.dp
 val spacingHuge: Dp = 48.dp
+
+val fontSizePrimary: TextUnit = 14.sp
+val fontSizesSecondary: TextUnit = 16.sp
+val fontSizesThird: TextUnit = 22.sp
+val fontSizesCaption: TextUnit = 12.sp
+val fontSizesSmall: TextUnit = 10.sp
+val fontSizesTitle: TextUnit = 24.sp
+val fontSizesTitleExtra: TextUnit = 32.sp
+
+val heightInput: Dp = 48.dp
+val heightDefaultButton: Dp = 48.dp
