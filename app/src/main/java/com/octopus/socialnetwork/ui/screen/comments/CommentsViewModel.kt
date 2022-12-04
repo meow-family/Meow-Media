96c8ed86d13f72a6003cdd2973eaedac16785b55
abdbashar <abdullahbashar98@gmail.com>
2022-12-04 19:51:17 +0300

    get post comments

--- app/src/main/java/com/octopus/socialnetwork/ui/screen/comments/CommentsViewModel.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/screen/comments/CommentsViewModel.kt
@@ -1,110 +1,61 @@
 package com.octopus.socialnetwork.ui.screen.comments
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.octopus.socialnetwork.ui.screen.comments.uistate.CommentDetailsUiState
+import androidx.lifecycle.viewModelScope
+import com.octopus.socialnetwork.domain.usecase.comments.GetPostCommentsUseCase
+import com.octopus.socialnetwork.ui.screen.comments.mapper.asCommentDetailsUiState
 import com.octopus.socialnetwork.ui.screen.comments.uistate.CommentsUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CommentsViewModel @Inject constructor() : ViewModel() {
+class CommentsViewModel @Inject constructor(
+    private val getPostCommentsUseCase: GetPostCommentsUseCase,
+) : ViewModel() {
 
     private val _state = MutableStateFlow(CommentsUiState())
     val state = _state.asStateFlow()
 
     init {
-        getFakeData()
+        getPostComments()
     }
 
-    fun getFakeData() {
-        _state.update {
-            it.copy(
-                comments = listOf(
-                    CommentDetailsUiState(
-
-                        "Abullah Bashar",
-                        "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png",
-                        "nice photo",
-                        "12",
-                        timeAgo = "3h",
-                    ),
-                    CommentDetailsUiState(
-
-                        "Abullah Bashar",
-                        "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png",
-                        "ninice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photoce photo",
-                        "12",
-                        "3h",
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar",
-                        "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png",
-                        "nice photo",
-                        "12",
-                        "3h",
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar",
-                        "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png",
-                        "nice photo",
-                        "12",
-                        "3h",
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar",
-                        "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png",
-                        "ninice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photonice photoce photo",
-                        "12",
-                        "3h",
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar", "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png", "nice photo", "12"
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar", "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png", "nice photo", "12"
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar", "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png", "nice photo", "12"
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar", "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png", "nice photo", "12"
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar", "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png", "nice photo", "12"
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar", "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png", "nice photo", "12"
-                    ),
-                    CommentDetailsUiState(
-                        "Abullah Bashar", "@98abdullah",
-                        "https://freepngimg.com/thumb/man/22654-6-man-thumb.png", "nice photo", "12"
+    private fun getPostComments() {
+        viewModelScope.launch {
+            try {
+                val postComments = getPostCommentsUseCase(
+                    currentUserId = 31,
+                    postId = 324,
+                    type = "post"
+                ).map { it.asCommentDetailsUiState() }
+                _state.update {
+                    it.copy(
+                        isSuccess = true,
+                        comments = postComments
                     )
+                }
+
+            } catch (e: Throwable) {
+                _state.update {
+                    it.copy(
+                        isError = true,
+                    )
+                }
+            }
+        }
+
+        fun onChangeTypingComment(newValue: String) {
+            _state.update {
+                it.copy(
+                    textFieldCommentState = it.textFieldCommentState.copy(text = newValue)
                 )
-            )
+            }
         }
+
     }
-
-    fun onChangeTypingComment(newValue: String) {
-        _state.update {
-            it.copy(
-                textFieldCommentState = it.textFieldCommentState.copy(text = newValue)
-            )
-        }
-    }
-
-
 }
