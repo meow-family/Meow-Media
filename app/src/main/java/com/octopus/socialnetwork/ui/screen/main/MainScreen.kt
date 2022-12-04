d5c07428aec625661f6b73d9b81f5ef23038d9c5
ameeramjed <pr.ameer98@gmail.com>
2022-12-04 18:55:36 +0300

    handle loading in home

--- app/src/main/java/com/octopus/socialnetwork/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/screen/main/MainScreen.kt
@@ -1,26 +1,28 @@
 package com.octopus.socialnetwork.ui.screen.main
 
+import android.annotation.SuppressLint
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.octopus.socialnetwork.ui.screen.home.HomeScreen
-import com.octopus.socialnetwork.ui.screen.register.RegisterScreen
+import com.octopus.socialnetwork.ui.screen.notifications.NotificationsScreen
 import com.octopus.socialnetwork.ui.theme.SocialNetworkTheme
 
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 @ExperimentalPagerApi
 @ExperimentalMaterialApi
 fun SocialNetworkApp() {
     SocialNetworkTheme {
         Scaffold {
-            //  HomeScreen()
-            RegisterScreen()
-//            HomeScreen()
+
+            HomeScreen()
             // RegisterScreen()
-//            LoginScreen()
+            // LoginScreen()
+            // NotificationsScreen()
         }
     }
 
