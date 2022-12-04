5da2696e6a6782ece74dfb64b0de7edff9186ed4
ameeramjed <pr.ameer98@gmail.com>
2022-12-04 17:46:27 +0300

    improve input birthday picker

--- app/src/main/java/com/octopus/socialnetwork/ui/composable/register/SecondStepRegistration.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/composable/register/SecondStepRegistration.kt
@@ -1,10 +1,15 @@
 package com.octopus.socialnetwork.ui.composable.register
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonDefaults.elevation
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarMonth
@@ -15,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
@@ -23,6 +29,9 @@ import com.octopus.socialnetwork.ui.composable.InputTextField
 import com.octopus.socialnetwork.ui.composable.SpacerVertical16
 import com.octopus.socialnetwork.ui.composable.rememberDatePicker
 import com.octopus.socialnetwork.ui.screen.register.uistate.UserInfoFormUiState
+import com.octopus.socialnetwork.ui.theme.heightDefaultButton
+import com.octopus.socialnetwork.ui.theme.spacingSmall
+import com.octopus.socialnetwork.ui.theme.zero
 
 
 @Composable
@@ -78,22 +87,33 @@ fun SecondStepRegistration(
         )
         SpacerVertical16()
 
-        InputTextField(
-            modifier = Modifier.clickable { datePicker.show() },
-            isReadOnly = true,
-            placeholder = stringResource(R.string.birthday),
-            icon = Icons.Default.CalendarMonth,
-            action = ImeAction.Done,
-            value = userInfoForm.birthDate.text,
-            onValueChange = onChangeBirthday,
-        )
-        Button(
-            onClick = { datePicker.show() },
-        ) {
+        Box() {
+            InputTextField(
+                modifier = Modifier.clickable(enabled = true, onClick = { datePicker.show() }),
+                isReadOnly = true,
+                placeholder = stringResource(R.string.birthday),
+                icon = Icons.Default.CalendarMonth,
+                action = ImeAction.Done,
+                value = userInfoForm.birthDate.text,
+                onValueChange = onChangeBirthday,
+            )
+            Button(
+                modifier = Modifier
+                    .height(heightDefaultButton)
+                    .fillMaxWidth()
+                    .padding(horizontal = spacingSmall),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Transparent
+                ),
+                elevation = elevation(
+                    defaultElevation = zero
+                ),
+                onClick = { datePicker.show() },
+            ) {}
+
         }
 
 
-//
     }
 }
 
