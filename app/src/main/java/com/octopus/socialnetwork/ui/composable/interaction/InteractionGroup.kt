0f3cc37699ef91f23a15c1966d73a0f583982c80
Wesam Nabeel <wesamezio@gmail.com>
2022-12-07 19:35:19 +0300

    prepare interaction group compose

---
+++ app/src/main/java/com/octopus/socialnetwork/ui/composable/interaction/InteractionGroup.kt
@@ -0,0 +1,22 @@
+package com.octopus.socialnetwork.ui.composable.interaction
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import com.octopus.socialnetwork.ui.composable.SpaceVertically8dp
+
+@Composable
+fun interactionGroup(
+    interactions: List<@Composable () -> Unit>
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        interactions.forEach { interactionIcon ->
+            interactionIcon()
+            SpaceVertically8dp()
+        }
+    }
+}
