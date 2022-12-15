32872bd72a5c1d37117409f5c5a6f31a32980d19
Wesam Nabeel <wesamezio@gmail.com>
2022-12-15 19:54:30 +0300

    remove unnecessary code

--- app/src/main/java/com/octopus/socialnetwork/ui/screen/search/SearchViewModel.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/screen/search/SearchViewModel.kt
@@ -2,15 +2,11 @@ package com.octopus.socialnetwork.ui.screen.search
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.octopus.socialnetwork.domain.usecase.search.SearchUseCase
-import com.octopus.socialnetwork.ui.screen.notifications.state.NotificationsUiState
 import com.octopus.socialnetwork.ui.screen.search.state.SearchUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,32 +19,10 @@ class SearchViewModel @Inject constructor(
     val state = _state.asStateFlow()
 
     init {
-        getNotifications()
+        search()
     }
 
-    private fun getNotifications() {
-//        viewModelScope.launch {
-//            try {
-//                val searchItem =
-//                    searchUseCase.invoke() { it.toNotificationsUiState() }
-//
-//                _state.update {
-//                    it.copy(
-//                        items = searchItem,
-//                        isLoading = false,
-//                        isError = false,
-//                    )
-//                }
-//
-//            } catch (e: Exception) {
-//                _state.update {
-//                    it.copy(
-//                        isLoading = false,
-//                        isError = true
-//                    )
-//                }
-//            }
-//        }
+    private fun search() {
 
     }
 
