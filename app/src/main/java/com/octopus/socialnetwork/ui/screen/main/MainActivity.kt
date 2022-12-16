2d821e5bb02862f217398d582dee48e1e3bafc90
Bahaa <Bahaa.Elramlawi300@gmail.com>
2022-12-16 17:27:25 +0200

    add splash screen implementation & make theme for it

--- app/src/main/java/com/octopus/socialnetwork/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/octopus/socialnetwork/ui/screen/main/MainActivity.kt
@@ -3,13 +3,21 @@ package com.octopus.socialnetwork.ui.screen.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             SocialNetworkApp()
         }
