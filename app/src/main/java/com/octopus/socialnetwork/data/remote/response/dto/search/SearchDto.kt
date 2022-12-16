0eea4a0085b085b22d433eefdb8f1ab22371a81d
Wesam Nabeel <wesamezio@gmail.com>
2022-12-17 02:11:02 +0300

    fix empty list issue in search

--- app/src/main/java/com/octopus/socialnetwork/data/remote/response/dto/search/SearchDto.kt
+++ app/src/main/java/com/octopus/socialnetwork/data/remote/response/dto/search/SearchDto.kt
@@ -1,13 +1,9 @@
 package com.octopus.socialnetwork.data.remote.response.dto.search
 
 import com.google.gson.annotations.SerializedName
-import com.octopus.socialnetwork.data.remote.response.dto.notifications.GroupDto
-import com.octopus.socialnetwork.data.remote.response.dto.post.PostDto
 import com.octopus.socialnetwork.data.remote.response.dto.user.UserDto
 
 data class SearchDto(
     @SerializedName("users")
-    val users: List<UserDto>,
-    @SerializedName("groups")
-    val groups: List<GroupDto>,
+    val users: List<UserDto>?,
 )
--- app/src/main/java/com/octopus/socialnetwork/domain/mapper/search/SearchMapper.kt
+++ app/src/main/java/com/octopus/socialnetwork/domain/mapper/search/SearchMapper.kt
@@ -1,12 +1,11 @@
 package com.octopus.socialnetwork.domain.mapper.search
 
 import com.octopus.socialnetwork.data.remote.response.dto.search.SearchDto
-import com.octopus.socialnetwork.domain.mapper.notifications.toGroup
 import com.octopus.socialnetwork.domain.mapper.user.toUserDetails
 import com.octopus.socialnetwork.domain.model.search.Search
 
 fun SearchDto.toSearch(): Search {
     return Search(
-        users = users.map { it.toUserDetails() },
+        users = users?.map { it.toUserDetails() } ?: emptyList(),
     )
 }
