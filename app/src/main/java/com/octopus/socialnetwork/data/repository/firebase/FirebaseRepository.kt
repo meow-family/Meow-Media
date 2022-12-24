ff7160bc4b41ff02265aece508fd67a1aaf93361
ameeramjed <pr.ameer98@gmail.com>
2022-12-25 01:31:41 +0300

    clean up

--- app/src/main/java/com/octopus/socialnetwork/data/repository/firebase/FirebaseRepository.kt
+++ app/src/main/java/com/octopus/socialnetwork/data/repository/firebase/FirebaseRepository.kt
@@ -7,7 +7,5 @@ interface FirebaseRepository {
     suspend fun createUser(user: UserFirebaseDTO)
     suspend fun updateUser(user: UserFirebaseDTO)
     suspend fun updateUserToken(userId: String, token: String)
-    suspend fun getUserToken(userId: String): String?
-
     suspend fun getUser(userId: String): UserFirebase?
 }
