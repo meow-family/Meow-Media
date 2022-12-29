3f8623921dd5e5619c76cd3a87e5fe3390ef4070
abdbashar <abdullahbashar98@gmail.com>
2022-12-29 22:34:18 +0300

    implement insert profile details to database use case

---
+++ app/src/main/java/com/octopus/socialnetwork/domain/usecase/user/user_details/InsertProfileDetailsUseCase.kt
@@ -0,0 +1,14 @@
+package com.octopus.socialnetwork.domain.usecase.user.user_details
+
+import com.octopus.socialnetwork.data.repository.social.SocialRepository
+import com.octopus.socialnetwork.domain.usecase.authentication.FetchUserIdUseCase
+import javax.inject.Inject
+
+class InsertProfileDetailsUseCase @Inject constructor(
+    private val socialRepository: SocialRepository,
+    private val fetchUserIdUseCase: FetchUserIdUseCase,
+) {
+    suspend operator fun invoke() {
+        return socialRepository.insertProfileDetails(fetchUserIdUseCase())
+    }
+}
